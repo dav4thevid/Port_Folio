b0ddae4f2d2fb02c5878936b6eb1add0ee85abf8
Fabusuyi David <dav4thevid@gmail.com>
2019-05-25 22:45:58 +0100

    modified stylesheet.css

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,19 +2,26 @@ import React from "react";
 
 function Body() {
   return (
-    <div>
+    <div class="grid">
       <img
         src="https://i.ibb.co/xDRzPTM/22781184.jpg"
         alt="22781184"
         border="0"
-        className = "img_header"
+        className="img_header"
       />
-      <h1 className="font_style">
+      <header className="font_style">
         I'm Dave Davoucii, <br />
         a UI/UX Developer <br />
-        based in Lagos, <br />
-        Nigeria.
-      </h1>
+        based in Lagos, Nigeria.
+        <br />
+        <p>
+          Im <strong>Creative</strong> & <strong>Passionate</strong> <br />
+          about my <strong>Work,</strong> I stand on a <br />
+          sweet spot where design <br />& <strong>Code Intersect.</strong>
+        </p>
+        <button class = "btn btn-success btn-xs mr-4">PROJECTS</button>
+        <button class = "btn btn-info btn-xs">WORKFLOW</button>
+      </header>
     </div>
   );
 }
