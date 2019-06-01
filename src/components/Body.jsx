cce94738a899c97a151e2a59b05d05ee8c03be98
Fabusuyi David <dav4thevid@gmail.com>
2019-06-01 03:21:08 +0100

    modified body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Body() {
   return (
-    <div class=" container grid">
+    <div class="content container grid">
       <h1 className="header_style">
         I'm Dave Davoucii, <br />
         a UI/UX Developer <br />
@@ -20,9 +20,6 @@ function Body() {
         <button class="btn btn-primary btn-md pl-5 pr-5 mr-5">PROJECTS</button>
         <button class="btn btn-default btn-md pl-5 pr-5 border-dark">WORKFLOW</button>
       </div>
-      <div class = "header_image">
-      <img src="https://i.ibb.co/cXh6vYp/IMG-20160820-001615.jpg" alt="IMG-20160820-001615"/>
-    </div>
     </div>
    
   );
