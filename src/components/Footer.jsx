d64ade7e5aa0542b28767783b5f4ee73d3499141
Fabusuyi David <dav4thevid@gmail.com>
2019-06-11 11:13:37 +0100

    installed font-awesome

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,11 +6,12 @@ function Footer(){
             <div class = "container-fluid footer">
             <div class = "row">
             <footer class = "footer-header">
-            <h2 class = "footer-header">"Out of clutter, find simplicity. From discord, find harmony. In the middle of difficulty lies opportunity."</h2>
-            <h2 class = "footer-p">"Never confuse movement with action." - Ernest Hemingway</h2>
-            <ion-icon name="logo-github"></ion-icon>
-            <ion-icon name="logo-twitter"></ion-icon>
-            <ion-icon name="logo-facebook"></ion-icon>
+            <h2 class = "footer-header">"Out of clutter, find simplicity. from discord, find harmony. In the middle of difficulty lies opportunity. - Albert Einstern"</h2>
+            <h2 class = "footer-header">"Never confuse movement with action." - Ernest Hemingway</h2>
+            <FontAwesomeIcon icon="coffee" />
+            <FontAwesomeIcon icon="coffee" />
+            <FontAwesomeIcon icon="coffee" />
+
             </footer>
             </div>
             </div>
