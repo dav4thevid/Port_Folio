e0362d15f0ad77b14fffd53d722c899c7617c94a
Fabusuyi David <dav4thevid@gmail.com>
2019-06-14 01:26:35 +0100

    changes to about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,7 +5,7 @@ function AboutMe(){
         <div class = "container-fluid about-me" id = "aboutme">
            
             <h1 class= "about-me-heading">The only thing more dangerous than ignorance is arrogance.<span> - Albert Einstern.</span></h1>
-            <p class = "about-me-p">I am a <span>web developer</span> from Lagos Nigeria, I enjoy building everything from <span>small business website</span> to <span>rich intergrated web app</span>.As someone who enjoys finding solutions to difficult problems, <span>software development</span> was an obvious path.Im also a <span>Gamer </span>|  <span>Football Fanatic</span> |  <span>Rap Music Addict.</span> if you are a <span>business owner</span> seeking a <span>web presence</span> or an <span>employer</span> looking to hire. <span>Holla!!!</span></p>
+            <p class = "about-me-p">I am a <span>web developer</span> from Lagos Nigeria, I enjoy building everything from <span>small business website</span> to <span>rich intergrated web apps</span>.As someone who enjoys finding solutions to difficult problems, <span>software development</span> was an obvious path.Im also a <span>Gamer </span>|  <span>Football Fanatic</span> |  <span>Rap Music Addict.</span> if you are a <span>business owner</span> seeking a <span>web presence</span> or an <span>employer</span> looking to hire. <span>Holla!!!</span></p>
             <p class = "about-me-p"></p>
             <p class = "about-me-p"></p>
 
