940e31a261fc28c518e003b8824d4f81508aed89
davoucii <da4thevid@gmail.com>
2019-06-22 21:03:34 +0100

    modified image-body

--- src/components/PortFolio.jsx
+++ src/components/PortFolio.jsx
@@ -3,17 +3,20 @@ import React from "react";
 function PortFolio() {
   return (
     <div>
-      <div class="container port-folio " id = "portfolio">
+      <div class="container port-folio " id="portfolio">
         <div class="row">
           <div class="col-12">
-            <div class="col text-center effect">
+            <div class="col text-center">
               <h1 id="portfolio-h1">YelpCamp </h1>
               <p id="portfolio-p">
                 A Yelp Camp App, where users can add campgrounds, comments on
                 campgrounds and check prizing.
               </p>
             </div>
-            <a href="https://floating-sands-38542.herokuapp.com/" target="_blank">
+            <a
+              href="https://floating-sands-38542.herokuapp.com/"
+              target="_blank"
+            >
               <img
                 class="mx-auto d-block col-sm-12 col-md-12 col-lg-12 effect"
                 src="https://i.ibb.co/RpTQbnC/Capture.png"
@@ -26,12 +29,14 @@ function PortFolio() {
         <br />
         <div class="row">
           <div class="col-sm-12 col-md-6  ">
-            <div class="col text-center effect">
+            <div class="col text-center">
               <h1 id="portfolio-h1">RGB Game </h1>
               <p id="portfolio-p">
                 An RGB Game which helps players to memorize RGB colors. You
                 think you know RGB Colors?
-                <span id="portfolio-span"> Play this Game.</span>
+                <p>
+                  <span id="portfolio-span"> Play this Game.</span>
+                </p>
               </p>
             </div>
             <a href="https://codepen.io/dav4thevid/full/YdqBNx" target="_blank">
@@ -46,23 +51,28 @@ function PortFolio() {
 
           <br />
 
-          <div class="col-sm-12 col-md-6 ">
-            <div class="col text-center effect">
+          <div class="col-sm-12 col-md-6  ">
+            <div class="col text-center">
               <h1 id="portfolio-h1">Patatap-Clone </h1>
               <p id="portfolio-p">
                 Patatap is a portable animation and sound kit. With the touch of
                 a finger create melodies charged with moving shapes.
-                <span id="portfolio-span">
-                  {" "}
-                  Warning: contains flashing images.
-                </span>
+                <p>
+                  <span id="portfolio-span">
+                    {" "}
+                    Warning: contains flashing images.
+                  </span>
+                </p>
               </p>
             </div>
-            <a href="https://patatap-clonedavoucii.netlify.com/" target="_blank">
+            <a
+              href="https://patatap-clonedavoucii.netlify.com/"
+              target="_blank"
+            >
               <img
                 class="mx-auto d-block col-sm-12 col-md-12 col-lg-12 effect"
                 src="https://i.ibb.co/8XNd1Qb/p8lPXhe.png"
-                alt="p8lPXhe"
+                alt="RGB"
                 border="0"
               />
             </a>
@@ -70,15 +80,18 @@ function PortFolio() {
         </div>
 
         <br />
-        <div class="row">
+        <div class="row ">
           <div class="col-sm-12 col-md-6  ">
-            <div class="col text-center effect">
+            <div class="col text-center effect ">
               <h1 id="portfolio-h1">RestFull Blog App </h1>
               <p id="portfolio-p">
                 A CRUD Blog, Where users can Add,Edit and Delete Blog Posts
               </p>
             </div>
-            <a href="https://pacific-shelf-39954.herokuapp.com/blogs" target="_blank">
+            <a
+              href="https://pacific-shelf-39954.herokuapp.com/blogs"
+              target="_blank"
+            >
               <img
                 class="mx-auto d-block col-sm-12 col-md-12 col-lg-12 effect"
                 src="https://i.ibb.co/2v55XBN/Capture.png"
@@ -89,9 +102,20 @@ function PortFolio() {
           </div>
           <div class="col-sm-12 col-md-6 ">
             <div class="col contact-me well">
-              <h1 class= "pl-3">Do you like what you are seeing? Lets have a chat!</h1>
-            <p class= "pl-3">I’m always looking to expand my network, get engaged in new challenges, or just have a coffee!</p>
-            <a href = "mailto:dav4thevid@gmail.com"id = "email-me" class = " btn btn-success port-folio-button">Send me an E-mail</a>
+              <h1 class="pl-3">
+                Do you like what you are seeing? Lets have a chat!
+              </h1>
+              <p class="pl-3">
+                I’m always looking to expand my network, get engaged in new
+                challenges, or just have a coffee!
+              </p>
+              <a
+                href="mailto:dav4thevid@gmail.com"
+                id="email-me"
+                class=" btn btn-success port-folio-button"
+              >
+                Send me an E-mail
+              </a>
             </div>
           </div>
         </div>
